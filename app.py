1877b85e63fb4dc6b898352ba6a3ab4283067c7b
Bernardo <bernardoxie@gmail.com>
2025-04-13 14:01:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 messages = []
-=======
-from flask import Flask, render_template, request
-import requests
 
 app = Flask(__name__)
 
@@ -13,8 +9,6 @@ def process_input(link, message):
   print("Received link:", link)
   print("Received message:", message)
 
-
->>>>>>> 7fe5ae0 (get link and message)
 @app.route("/", methods=["GET", "POST"])
 def index():
   if request.method == "POST":
