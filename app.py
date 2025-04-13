8a00431de170c6b24319ef673f8734c150dcacbc
Bernardo <bernardoxie@gmail.com>
2025-04-13 13:55:31 -0400

    get link and message

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+<<<<<<< HEAD
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 messages = []
+=======
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def process_input(link, message):
+  print("Received link:", link)
+  print("Received message:", message)
+
+
+>>>>>>> 7fe5ae0 (get link and message)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+  if request.method == "POST":
+      link = request.form.get("link")
+      message = request.form.get("message")  
+      # Call the function
+      process_input(link, message)
+  return render_template("index.html")
